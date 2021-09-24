01b440c3c95d6a1aedd020c793fe6ccbc6e2e05e
Dulanji <dulanjis54@gmail.com>
2021-09-25 01:56:44 +0530

    animal ticket update

--- app/src/main/java/com/example/zooapp/Ticketbooking.java
+++ app/src/main/java/com/example/zooapp/Ticketbooking.java
@@ -1,5 +1,6 @@
 package com.example.zooapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
@@ -18,8 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -32,8 +36,12 @@ public class Ticketbooking extends AppCompatActivity {
         String userID,userid,email;
         TextView Prate_B;
         DatePickerDialog.OnDateSetListener setListener;
+        double child_t_Amount =0 ,adult_t_Amount = 0,fullAmount = 0;
+        long loc_adult_price,loc_child_price,for_adult_price,for_child_price;
         FirebaseAuth fauth = FirebaseAuth.getInstance();
-         DatabaseReference reff;
+         DatabaseReference reff,rates;
+        int number_child ;
+        int number_adult;
 
 
 
@@ -51,7 +59,8 @@ public class Ticketbooking extends AppCompatActivity {
     final int month =calendar.get(Calendar.MONTH);
     final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-
+       //create database connection to ticket price table
+        rates = FirebaseDatabase.getInstance().getReference("TicketPrice").child("PriceRate");
 
 
         etDate.setOnClickListener(new View.OnClickListener() {
@@ -102,6 +111,32 @@ public class Ticketbooking extends AppCompatActivity {
 
         reff= FirebaseDatabase.getInstance().getReference("TicketBooking").child("TicketBookDetails");
 
+
+        rates.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //  for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+
+                String Option1 = String.valueOf(snapshot.child("Foreign_adult").getValue(Long.class));
+                String Option2= String.valueOf(snapshot.child("Foreign_child").getValue(Long.class));
+                String Option3 = String.valueOf(snapshot.child("Local_adult").getValue(Long.class));
+                String Option4 = String.valueOf(snapshot.child("Local_child").getValue(Long.class));
+
+
+                for_adult_price = Long.parseLong(Option1);
+                for_child_price = Long.parseLong(Option2);
+                loc_adult_price = Long.parseLong(Option3);
+                loc_child_price= Long.parseLong(Option4);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         STbook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { insertS_Book();
@@ -109,6 +144,7 @@ public class Ticketbooking extends AppCompatActivity {
             }
         });
     }
+
     private void insertS_Book() {
         String Snational = S_National.getSelectedItem().toString();
         String Sadult = S_Adult.getText().toString();
@@ -129,9 +165,41 @@ public class Ticketbooking extends AppCompatActivity {
             return;
         }
 
+        //String value convert to integer
+        if(S_Adult.isEnabled())
+        {
+            number_adult= Integer.parseInt(Sadult);
+
+        }else{
+            number_adult = 0;
+        }
+        if(S_Child.isEnabled())
+        {
+            number_child =Integer.parseInt(Schild);
+
+        }else{
+            number_child = 0;
+        }
+
+        //calculation
+        if (Snational.equals("Foreign"))
+        {
+             child_t_Amount= number_child * for_child_price;
+            adult_t_Amount = number_adult * for_adult_price;
+        }
+        else if(Snational.equals("Local"))
+        {
+            child_t_Amount= number_child * loc_child_price;
+            adult_t_Amount = number_adult * loc_adult_price;
+        }
+        else{
+            child_t_Amount= 0;
+            adult_t_Amount = 0;
+        }
+        fullAmount = child_t_Amount+ adult_t_Amount;
 
 
-        Ticket S =new Ticket (Snational,Sadult,Schild,Sdate,userID);
+        Ticket S =new Ticket (Snational,Sadult,Schild,Sdate,number_child,number_adult,child_t_Amount,adult_t_Amount,fullAmount,userID);
 
         reff.push().setValue(S);
         Toast.makeText(Ticketbooking.this, "Ticket Booking successful", Toast.LENGTH_LONG).show();
